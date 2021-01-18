a6f4f8706990051b5afe8cf187423950aee3e503
Bernardo F. B. Braga <bernardo.braga@intelie.com.br>
2021-01-18 00:04:20 -0300

    relembrando conceitos de cpp

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -4,6 +4,24 @@
 int main()
 {
     Vector3D* v = new Vector3D(1,2,3);
-    std::cout << v->x;
+    // *v: struct apontado por v 
+    // abaixo, esse struct é copiado para um novo struct t
+    Vector3D t = *v;
+
+    // Altera o struct apontado por v, mas não altera t
+    *v *= 3;
+
+    // prova que os valores são diferentes
+    std::cout << v->x << std::endl;
+    std::cout << t.x << std::endl;
+
+    // prova que os endereços são diferentes
+    // &t: endereço de t
+    std::cout << &t << std::endl;
+    std::cout << v << std::endl;
+
+    // acessando valor pelo operador [] sobrecarregado
+    std::cout << (*v)[0] << std::endl;
+    std::cout << t[0] << std::endl;
     return 0;
 };
