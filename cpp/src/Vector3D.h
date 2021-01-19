1864af34dc64f60a89407aaea42173858d819c4a
Bernardo F. B. Braga <bernardo.braga@intelie.com.br>
2021-01-18 22:34:15 -0300

    Comentários sobre o funcionamento da linguagem e mais métodos de vetor

--- cpp/src/Vector3D.h
+++ cpp/src/Vector3D.h
@@ -1,14 +1,17 @@
 #ifndef VECTOR_3D
 #define VECTOR_3D
 
+#include <iostream>
 #include <cmath>
 
 struct Vector3D
 {
     float x, y, z;
 
+    // construtor default permite instanciar sem inicializar
     Vector3D() = default;
 
+    // construtor
     Vector3D(float a, float b, float c)
     {
         x = a;
@@ -16,6 +19,7 @@ struct Vector3D
         z = c;
     }
 
+    // float & no retorno indica que retorna valor por referência
     float &operator[](int i)
     {
         return ((&x)[i]);
@@ -42,22 +46,58 @@ struct Vector3D
         z *= s;
         return (*this);
     }
+
+    Vector3D &operator+=(const Vector3D &v)
+    {
+        x += v.x;
+        y += v.y;
+        z += v.z;
+        return (*this);
+    }
+
+    Vector3D &operator-=(const Vector3D &v)
+    {
+        x -= v.x;
+        y -= v.y;
+        z -= v.z;
+        return (*this);
+    }
 };
 
-inline Vector3D operator * (const Vector3D& v, float s){
-    return (Vector3D(v.x*s, v.y*s, v.z*s));
+std::ostream &operator<<(std::ostream &os, const Vector3D &v)
+{
+    return os << "[" << v.x << "," << v.y << "," << v.z << "]";
 }
 
-inline Vector3D operator / (const Vector3D& v, float s){
-    s = 1.0F /s;
-    return (Vector3D(v.x*s, v.y*s, v.z*s));
+inline Vector3D operator*(const Vector3D &v, float s)
+{
+    return (Vector3D(v.x * s, v.y * s, v.z * s));
 }
 
-inline Vector3D operator - (const Vector3D& v){
+inline Vector3D operator/(const Vector3D &v, float s)
+{
+    s = 1.0F / s;
+    return (Vector3D(v.x * s, v.y * s, v.z * s));
+}
+
+// para negativar um vetor
+inline Vector3D operator-(const Vector3D &v)
+{
     return (Vector3D(-v.x, -v.y, -v.z));
 }
 
-inline float Magnitude(const Vector3D& v){
+inline Vector3D operator+(const Vector3D &a, const Vector3D &b)
+{
+    return (Vector3D(a.x + b.x, a.y + b.y, a.z + b.z));
+}
+
+inline Vector3D operator-(const Vector3D &a, const Vector3D &b)
+{
+    return (Vector3D(a.x - b.x, a.y - b.y, a.z - b.z));
+}
+
+inline float Magnitude(const Vector3D &v)
+{
     return (sqrt(v.x * v.x + v.y * v.y + v.z * v.z));
 }
 #endif
--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -3,25 +3,45 @@
 
 int main()
 {
-    Vector3D* v = new Vector3D(1,2,3);
-    // *v: struct apontado por v 
-    // abaixo, esse struct é copiado para um novo struct t
-    Vector3D t = *v;
+    Vector3D *v1 = new Vector3D(1, 2, 3);
 
-    // Altera o struct apontado por v, mas não altera t
-    *v *= 3;
+    // Abaixo, esse struct é copiado para um novo struct v2
+    Vector3D v2 = *v1;
 
-    // prova que os valores são diferentes
-    std::cout << v->x << std::endl;
-    std::cout << t.x << std::endl;
+    std::cout << "Endereço de memória de V1:" << v1 << std::endl;
+    // &v2: endereço de v2
+    std::cout << "Endereço de memória de V2:" << &v2 << std::endl;
 
-    // prova que os endereços são diferentes
-    // &t: endereço de t
-    std::cout << &t << std::endl;
-    std::cout << v << std::endl;
+    (*v1)[0] = 2;
 
-    // acessando valor pelo operador [] sobrecarregado
-    std::cout << (*v)[0] << std::endl;
-    std::cout << t[0] << std::endl;
+    float x = (*v1)[0] = 1;
+    std::cout << "Valor de X:" << x << std::endl;
+    // *v1: struct apontado por v1
+    // Altera o struct apontado por v1, mas não altera v2
+    *v1 *= 3;
+
+    // Acessando valor de um pointeiro:, deferenciando o ponteiro e usando o operador [] sobrecarregado
+    std::cout << "Valor de V1:" <<
+        // normalmente
+        v1->x << " == " <<
+        // pegando o valor apontado pelo ponteiro v1 e acessando como valor
+        (*v1).x << " == " <<
+        // acessando como valor usando o operador sobrecarregado
+        (*v1)[0] << std::endl;
+    // Acessando valor de um struct:
+    std::cout << "Valor de V2:" <<
+        // Acessando o endereço de v2 e tratando como ponteiro
+        (&v2)->x << " == " <<
+        // diretamente como valor
+        v2.x << " == " <<
+        // usando o operador [] sobrecarregado
+        v2[0] << std::endl;
+
+    std::cout << "V1 :" << (*v1) << std::endl;
+    std::cout << "V2 :" << v2 << std::endl;
+    std::cout << "V1 - V2:" << (*v1) - v2 << std::endl;
+    std::cout << "V1 + V2:" << (*v1) + v2 << std::endl;
+    std::cout << "Magnitude of V2:" << Magnitude(v2) << std::endl;
+
     return 0;
 };
